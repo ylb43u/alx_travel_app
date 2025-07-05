8bfc4b97008c5f3fcd325d498c246c6cffe63573
RachidJedata <jedatarachid@gmail.com>
2025-07-05 14:23:32 +0100

    feat:Django Project Setup with API Documentation and Database Configuration

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,28 +1,23 @@
-from django.contrib import admin
-from django.urls import path, re_path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+"""
+URL configuration for alx_travel_app project.
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel API",
-      default_version='v1',
-      description="API documentation for ALX Travel App",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # Your app routes (example)
-    # path('api/', include('trips.urls')),
-
-    # Swagger docs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-    # Optional: JSON/YAML endpoints
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("admin/", admin.site.urls),
 ]
